04a50da93979a2bf1d7fc823636ea42890c8755b
ryokf <ryokhrisnaf@gmail.com>
2025-12-20 14:34:18 +0700

    feat: Add member management, refine policy detail retrieval, and expand test coverage.

--- src/ParisurePool.sol
+++ src/ParisurePool.sol
@@ -25,15 +25,16 @@ contract ParisurePool {
         i_owner = owner;
     }
 
-    uint256 private s_policyCount = 0;
-    mapping(uint256 idPolicy => PoolLib.Policy) s_policyList;
+    uint256 public s_policyCount = 0;
+    mapping(uint256 idPolicy => PoolLib.Policy) private s_policyList;
 
-    mapping(address => PoolLib.Member) private s_members;
+    mapping(uint256 id => address memberAddress) private s_memberId;
+    mapping(address => PoolLib.Member) public s_members;
 
-    PoolLib.Claim[] private s_claims;
+    PoolLib.Claim[] public s_claims;
 
     mapping(uint256 id => mapping(address member => bool hasVoted))
-        private s_voters;
+        public s_voters;
 
     function createPolicy(
         string memory _name,
@@ -50,9 +51,33 @@ contract ParisurePool {
         s_policyCount++;
     }
 
-    function getPolicy(
+    function getPolicyDetail(
         uint256 _policyId
     ) public view returns (PoolLib.Policy memory) {
         return s_policyList[_policyId];
+    }
+
+    function memberJoin(
+        address memberAddress,
+        uint256 joinedAt,
+        uint256 expiredDate,
+        uint256 policyId
+    ) public {
+        s_members[memberAddress] = PoolLib.Member(
+            true,
+            joinedAt,
+            expiredDate,
+            policyId
+        );
+    }
+
+    function getMember(
+        uint256 _id
+    ) public view returns (PoolLib.Member memory) {
+        address memberAddress = s_memberId[_id];
+
+        PoolLib.Member memory memberDetail = s_members[memberAddress];
+
+        return memberDetail;
     }
 }
--- test/ParisurePoolTest.t.sol
+++ test/ParisurePoolTest.t.sol
@@ -17,6 +17,29 @@ contract ParisurePoolTest is Test {
     }
 
     function testCreatePolicy() public {
-        pool.createPolicy("perlindungan kilat", 1, 1 wei, true);
+        pool.createPolicy("perlindungan kilat", 1, 10 gwei, true);
+
+        PoolLib.Policy memory newPool = pool.getPolicyDetail(0);
+
+        assertEq(newPool.name, "perlindungan kilat");
+        assertEq(newPool.duration, 1);
+        assertEq(newPool.price, 10 gwei);
+        assertEq(newPool.isActive, true);
+    }
+
+    function testGetAllPolicy() public {
+        pool.createPolicy("perlindungan kilat", 1, 10 gwei, true);
+        pool.createPolicy("perlindungan aman", 7, 50 gwei, true);
+        pool.createPolicy("perlindungan ekstra", 30, 250 gwei, true);
+
+        uint256 policyCount = pool.s_policyCount();
+
+        for (uint256 i = 0; i < policyCount; i++) {
+            PoolLib.Policy memory policy = pool.getPolicyDetail(i);
+            console.log(policy.name);
+            console.log(policy.duration);
+            console.log(policy.price);
+            console.log(policy.isActive);
+        }
     }
 }
