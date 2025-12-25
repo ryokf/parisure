c35b4777594ce8086a3f42d29877e2f7c8b97c9b
ryokf <ryokhrisnaf@gmail.com>
2025-12-25 13:36:16 +0700

    feat: Add Foundry chain support, deploy script, and initial contract address for local development setup.

--- frontend/config/index.tsx
+++ frontend/config/index.tsx
@@ -1,14 +1,14 @@
 'use client'
 
 import { getDefaultConfig } from "@rainbow-me/rainbowkit"
-import { sepolia } from "viem/chains"
+import { foundry, sepolia } from "viem/chains"
 
 const projectId = "rhs1wvvxx74p5llz"
 
 const config = getDefaultConfig({
     appName: "Parisure",
     projectId: projectId,
-    chains: [sepolia],
+    chains: [foundry, sepolia],
     ssr: true
 })
 
---
+++ script/DeployParisure.s.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.30;
+
+import {Script} from "forge-std/Script.sol";
+import {PoolFactory} from "../src/PoolFactory.sol";
+
+contract DeployParisure is Script {
+    function run() public returns (PoolFactory) {
+        uint256 deployer = vm.envOr(
+            "PRIVATE_KEY_ANVIL",
+            uint256(
+                0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+            )
+        );
+
+        vm.startBroadcast(deployer);
+
+        PoolFactory poolFactory = new PoolFactory();
+
+        vm.stopBroadcast();
+
+        return poolFactory;
+    }
+}
